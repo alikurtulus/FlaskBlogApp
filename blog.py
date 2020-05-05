1d17fd53a1c3911663ac629ace48f6f443dda185
alikurtulush92 <kopanarya92@gmail.com>
2020-05-05 23:57:21 +0100

    Added login and sign up

--- blog.py
+++ blog.py
@@ -4,7 +4,8 @@ from wtforms import Form, StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-
+app.secret_key = "flaskablog"
+# We create connection between
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
@@ -24,6 +25,17 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField("PasswordConfirmation:")
 
+
+class LoginForm(Form):
+    email = StringField('Email',validators=[
+        validators.Length(min= 4, max = 25),
+        validators.Email('Please enter correct email ...'),
+        validators.DataRequired(message = 'Please enter an email ...')
+        ])
+    password = PasswordField('Password',validators=[
+        validators.Length(min = 4, max = 25),
+        validators.DataRequired(message='Please enter a password')
+    ])    
 
 mysql = MySQL(app)
 
@@ -64,11 +76,57 @@ def register():
         mysql.connection.commit()
 
         cursor.close()
-
-        return redirect(url_for('index'))
+        flash("Succesfuly Signed Up!!","success")
+        return redirect(url_for('login'))
 
     else:
         return render_template('register.html',form = form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password_entered = form.password.data
+
+        cursor = mysql.connection.cursor()
+        query = "SELECT * FROM users WHERE email= %s"
+
+        result = cursor.execute(query,(email,))
+
+        if result > 0:
+            data = cursor.fetchone()
+            real_password = data['password']
+            username = data['username']
+            if sha256_crypt.verify(password_entered,real_password):
+                flash('Succesfully login','success')
+                session['logged_in'] = True
+                session['username'] = username
+                return redirect(url_for('index'))
+            else:
+                flash('Invalid inputs','danger')
+                return redirect(url_for('login'), form = form)
+
+
+        else:
+            flash('This user does not exist ...', 'danger')
+            return redirect(url_for('login'))
+
+    else:
+        return render_template('login.html', form = form)
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+    
 
 
 if __name__ == '__main__':
